8b2cc360baf710e43c34bd9b3aeb1091e42d6d76
Kody King <kody.j.king@gmail.com>
2019-03-16 06:41:18 -0700

    Implemented voronoi cell coloring.

--- Voronoi/voronoi.js
+++ Voronoi/voronoi.js
@@ -19,46 +19,49 @@ class Vector {
     get unit() { return this.multiply(1 / this.length) }
     get leftNormal() { return new Vector(-this.y, this.x) }
     get rightNormal() { return new Vector(this.y, -this.x) }
-    add(addedVector) { return new Vector(this.x + addedVector.x, this.y + addedVector.y) }
-    subtract(subtractedVector) { return new Vector(this.x - subtractedVector.x, this.y - subtractedVector.y) }
+    get angle() { return Math.atan2(this.y, this.x) }
+    add(other) { return new Vector(this.x + other.x, this.y + other.y) }
+    subtract(other) { return new Vector(this.x - other.x, this.y - other.y) }
     dot(other) { return this.x * other.x + this.y * other.y }
+    cross(other) { return this.x * other.y - this.y * other.x }
     multiply(scale) { return new Vector(this.x * scale, this.y * scale) }
     lerp(other, t) { return this.multiply(1 - t).add(other.multiply(t)) }
 }
+
  class BoundryLine {
-    constructor(point, heading, rightRegion, leftRegion) {
+    constructor(point, heading, leftRegion, rightRegion) {
         this.point = point
         this.heading = heading
-        this.leftRegion = rightRegion
-        this.rightRegion = leftRegion
-        this.forwardBound = Infinity
-        this.backwardBound = -Infinity
+        this.leftRegion = leftRegion
+        this.rightRegion = rightRegion
+        this.forward = Infinity
+        this.backward = -Infinity
     }
 
     clip(point, normal) {
         let projection = this.heading.dot(point.subtract(this.point))
         let forward = this.heading.dot(normal) >= 0
         if (forward)
-            this.forwardBound = Math.min(this.forwardBound, projection)
+            this.forward = Math.min(this.forward, projection)
         else
-            this.backwardBound = Math.max(this.backwardBound, projection)
+            this.backward = Math.max(this.backward, projection)
     }
 
     get fullyClipped() {
-        return this.forwardBound < this.backwardBound
+        return this.forward < this.backward
     }
 
     regionNormal(region) {
-        return region == this.rightRegion ? this.heading.rightNormal : this.heading.leftNormal
+        return region == this.leftRegion ? this.heading.rightNormal : this.heading.leftNormal
     }
 
     get forwardPoint() {
-        let distance = Math.min(this.forwardBound, 1000)
+        let distance = Math.min(this.forward, 10000)
         return this.point.add(this.heading.multiply(distance))
     }
 
     get backwardPoint() {
-        let distance = Math.max(this.backwardBound, -1000)
+        let distance = Math.max(this.backward, -10000)
         return this.point.add(this.heading.multiply(distance))
     }
  }
@@ -81,8 +84,13 @@ class Vector {
             }
         }
 
+        this.idCounter = 0
+
         this.addPairBoundries()
-        this.removeClippedLines()
+        this.lines = this.lines.filter((line) => !line.fullyClipped)
+        this.pointsToLines.forEach((lines, i) => {
+            this.pointsToLines[i] = lines.filter((line) => !line.fullyClipped)
+        })
     }
 
     addPairBoundries() {
@@ -92,16 +100,9 @@ class Vector {
                 let pj = this.points[j]
                 let midPoint = pi.lerp(pj, 0.5)
                 let heading = pj.subtract(pi).rightNormal.unit
-                this.addLine(new BoundryLine(midPoint, heading, i, j))
+                this.addLine(new BoundryLine(midPoint, heading, j, i))
             }
         }
-    }
-
-    removeClippedLines() {
-        for (let i = 0; i < this.lines.length; i++)
-            if (this.lines[i].fullyClipped)
-                this.lines[i] = null
-        this.lines = this.lines.filter((e) => e != null)
     }
 
     addLine(line) {
@@ -119,7 +120,25 @@ class Vector {
 
     clip(a, b, region) {
         let point = pointOfIntersection(a.point, a.heading, b.point, b.heading)
+        let pointId = this.idCounter++
         a.clip(point, b.regionNormal(region))
         b.clip(point, a.regionNormal(region))
+    }
+
+    get polygons() {
+        return this.pointsToLines.map((lines, i) => this.polygon(lines, i))
+    }
+
+    polygon(lines, region) {
+        let points = []
+        for (let line of lines) {
+            points.push(line.forwardPoint)
+            points.push(line.backwardPoint)
+        }
+
+        let centerPoint = this.points[region]
+        points.sort((a, b) => a.subtract(centerPoint).angle - b.subtract(centerPoint).angle )
+
+        return points
     }
  }
